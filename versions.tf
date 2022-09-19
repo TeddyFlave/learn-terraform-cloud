d0dbf120121c2b8e6417af7ef1577a1bb5d7eb62
TeddyFlave <RooseveltAkume7@Gmail.com>
2022-09-19 05:51:58 -0400

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-
+/*
   cloud {
     organization = "TerraformTrainingOrganization"
 
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.31.0"
+      version = "3.28.0"
     }
   }
 
